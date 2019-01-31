c52a4eb395694e9c0cb2e7a6e7c8a2d4e26e1227
kevinalexandersurjadi <kevin.alexander@oval.com>
2019-01-31 17:24:40 +0700

    Pokemon Repo

---
+++ pokemon_dart/lib/repo/repo.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:pokemon_dart/repo/model.dart';
+import 'package:pokemon_dart/util/pokemon_client.dart';
+
+class PokemonRepo extends PokemonClient {
+  @override
+  String get endpoint => "pokemon";
+
+  Future<List<Pokemon>> getAll() async {
+    final response = await httpGet();
+    final List<Map<String, dynamic>> results = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      List<Pokemon> listPokemon = List();
+      results.map((element) => listPokemon.add(Pokemon.fromJson(element)));
+      return listPokemon;
+    } else {
+      throw "ERROR";
+    }
+  }
+}
